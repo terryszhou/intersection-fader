09c1b2af5ff984f2ca5f46a0cb904440f7c41486
terryszhou <terryszhou@gmail.com>
2022-01-28 22:36:39 -0600

    Restyled for side-by-side comparison

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Box, Text } from '@chakra-ui/react';
+import { Box, Text, HStack, Flex } from '@chakra-ui/react';
 
 import './App.css';
 import { colors } from './css-colors';
@@ -9,31 +9,35 @@ export const App = () => {
   return (
     <div className="App">
       <h1>All the CSS Colors!</h1>
-
-      {/* {colors.map(color => (
-        <FadeInSection>
-          <div className="box" style={{ backgroundColor: color }}>
-            <span>{color}</span>
-          </div>
-        </FadeInSection>
-      ))} */}
-
-      {colors.map(color => (
-        <FadeInSectionChakra>
-          <Box
-            p={50}
-            m={"20px 0"}
-            bgColor={color}>
-            <Text
-              bgColor={"rgba(255,255,255,0.5)"}
-              display={"inline-block"}
-              p={2}
-              borderRadius={3}>
-              {color}
-            </Text>
-          </Box>
-        </FadeInSectionChakra>
-      ))}
+      <Flex>
+        <div style={{ width: "50%", margin: "0 2px" }}>
+          {colors.map(color => (
+            <FadeInSection>
+              <div className="box" style={{ backgroundColor: color }}>
+                <span>{color}</span>
+              </div>
+            </FadeInSection>
+          ))}
+        </div>
+        <Box w={"50%"} mx={2}>
+          {colors.map(color => (
+            <FadeInSectionChakra>
+              <Box
+                p={"50px"}
+                m={"20px 0"}
+                bgColor={color}>
+                <Text
+                  bgColor={"rgba(255,255,255,0.5)"}
+                  display={"inline-block"}
+                  p={"5px"}
+                  borderRadius={3}>
+                  {color}
+                </Text>
+              </Box>
+            </FadeInSectionChakra>
+          ))}
+        </Box>
+      </Flex>
     </div>
   );
 };
