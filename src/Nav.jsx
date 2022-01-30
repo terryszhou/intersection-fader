b52d145990e6a226612012aeeba86e3e03804540
terryszhou <terryszhou@gmail.com>
2022-01-29 18:48:24 -0600

    Navline added

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 
-export const Nav = () => (
-  <nav style={styles.navbar}>
-    <div style={{ float: "right" }}>
-      <NavLink to="/" exact style={styles.navlink}>Vertical</NavLink>
-      <NavLink to="/horizontal" style={styles.navlink}>Horizontal</NavLink>
-    </div>
-  </nav>
-);
+export const Nav = () => {
+  const [href, setHref] = useState("vertical");
+
+  return (
+    <nav style={styles.navbar}>
+      <div style={{ float: "right" }}>
+        <NavLink
+          to="/"
+          exact
+          className="navlink"
+          onClick={() => setHref("vertical")}>
+          Vertical
+        </NavLink>
+        <NavLink
+          to="/horizontal"
+          className="navlink"
+          onClick={() => setHref("horizontal")}>
+          Horizontal
+        </NavLink>
+        <div style={{
+          position: "relative",
+          height: "2px",
+          backgroundColor: "white",
+          borderRadius: "15px",
+          width: href === "vertical" ? 65 : 84, 
+          left: href === "vertical" ? 8 : 85,
+          transitionDuration: "0.2s",
+        }}/>
+      </div>
+    </nav>
+  );
+};
 
 const styles = {
   navbar: {
@@ -17,10 +41,5 @@ const styles = {
     position: "fixed",
     width: "100vw",
     zIndex: 2,
-  },
-  navlink: {
-    margin: "0 10px",
-    fontSize: 24,
-    color: "white",
   },
 };
